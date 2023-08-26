4cc61aadb97354ba84776a3155219025a6df2b42
Zubayr2110 <webproject2110@gmail.com>
2023-08-26 21:19:48 +0500

    done

---
+++ class.js
@@ -0,0 +1,59 @@
+//  1
+
+// class car {
+//     constructor(name, position, weight, speed, price, enginetype){
+//         this.name = name;
+//         this.position = position;
+//         this.weight = weight;
+//         this.speed = speed;
+//         this.price = price;
+//         this.enginetype = enginetype;
+//     }
+//     getInfo(){
+//         console.log(this.name + ' ' + this.price);
+//     }
+// }
+
+// let Gentra = new car('Gentra', 3, '1.5 tonna', 220, '18.500 y.e', 'benzin')
+
+// let Nexia3 = new car('Nexia 3', 2, '1 tonna', 180, '13.800 y.e', 'benzin')
+
+// let Cobalt = new car('Cobalt', 4, '1140 kg', 170, '14.800 y.e', 'benzin')
+
+// let Spark = new car('Spark', 4, '950 kg', 150, '11.500 y.e', 'benzin')
+
+// let Matiz = new car('Matiz', 2, '730 kg', 150, '5 000 y.e', 'benzin')
+
+// let damas = new car('Damas', 2, '850 kg', 150, '10.000 y.e', 'benzin')
+
+
+
+
+
+
+//   2
+
+// class Phone {
+//     constructor(name, price, display, camera, ram){
+//         this.name = name;
+//         this.price = price;
+//         this.display = display;
+//         this.camera = camera;
+//         this.ram = ram;
+//     }
+//     phone(){
+//         console.log(this.name + ' ' + this.price);
+//     }
+// }
+// let iphone14 = new Phone('Iphone 14 pro', '10 053 000 sum', '2000 nit', '48 MP')
+
+// let samsungs22 = new Phone('Samsung S22', '$950', '1300 nit', '10 MP', '8 GB')
+
+// let asusrogphone = new Phone('Asus Rog Phone 7', '$640', '1000 nit', '50 MP', '12 GB')
+
+// let pocox5 = new Phone('Poco X5 PRO', '$350', '1500 nit', '108 MP', '8 GB')
+
+
+
+
+//  3
---
+++ switch.js
@@ -0,0 +1,108 @@
+// let n = 90
+// switch(n){
+//     case 90: console.log('Beeline');
+//     case 99: console.log('Uzmobile');
+//     case 71: console.log('Damashniy telefon');
+//     case 33: console.log('Humans');
+//     case 93: console.log('Ucell');
+//     case 98: console.log('Perfectum');
+//     default: console.log('not found');
+// }  1 - masala
+
+
+// let k = 5
+// switch(k){
+//     case 1: console.log('yomon');
+//     case 2: console.log('qoniqarsiz');
+//     case 3: console.log('qoniqarli');
+//     case 4: console.log('yahshi');
+//     case 5: console.log('alo');
+//     default: console.log('not found');
+// }    2 - masala
+
+
+
+// let n = 'macbook'
+// switch(n){
+//     case 'victus': console.log("HP Victus Gaming Laptop price $700");break
+//     case 'macbook': console.log("MacBook air M2 chip price $1099");break
+//     case 'asus': console.log("Asus Strix Scar price $4000");break
+//     default: console.log('not found');
+// }  3 - masala
+
+
+
+
+
+// let k = 3
+// switch(k){
+//     case 1: console.log('Dushanba');
+//     case 2: console.log('Seshanba');
+//     case 3: console.log('Choprshanba');
+//     case 4: console.log('Payshanba');
+//     case 5: console.log('Juma');
+//     case 6: console.log('Shanba');
+//     case 7: console.log('Yashanba');
+//     default: console.log('not found');
+// }   4 - masala
+
+
+
+// let m = 4
+// switch(m){
+//     case 1: console.log('qish');break
+//     case 2: console.log('qish');break
+//     case 3: console.log('bahor');
+//     case 4: console.log('bahor');
+//     case 5: console.log('bahor');
+//     case 6: console.log('yoz');break
+//     case 7: console.log('yoz');break
+//     case 8: console.log('yoz');break
+//     case 9: console.log('kuz');break
+//     case 10: console.log('kuz');break
+//     case 11: console.log('kuz');break
+//     case 12: console.log('qish');break
+//     default: console.log("bunaqa oy yo'q");
+// }  5 - masala
+
+
+
+// let m = 3
+// switch(m){
+//     case 1: console.log('31 kun bor');break
+//     case 2: console.log('28 kun bor');break
+//     case 3: console.log('31 kun bor');break
+//     case 4: console.log('30 kun bor');break
+//     case 5: console.log('31 kun bor');break
+//     case 6: console.log('30 kun bor');break
+//     case 7: console.log('31 kun bor');break
+//     case 8: console.log('31 kun bor');break
+//     case 9: console.log('30 kun bor');break
+//     case 10: console.log('31 kun bor');break
+//     case 11: console.log('30 kun bor');break
+//     case 12: console.log('31 kun bor');break
+//     default: console.log("bunaqa oy yo'q");
+// }   6 - masala
+
+
+// let a = 5, b = 3, c = '+'
+// switch(c){
+//     case '+': console.log(a + b);break
+//     case '-': console.log(a - b);break
+//     case '*': console.log(a * b);break
+//     case '/': console.log(a / b);break
+//     case '%': console.log(a % b);break
+//     default: console.log('bunaqa amal mavjudmas');
+// }   7 - masala
+
+
+
+// let k = 3
+// switch(k){
+//     case 1: console.log('yomon');
+//     case 2: console.log('qoniqarsiz');
+//     case 3: console.log('qoniqarli');
+//     case 4: console.log('yahshi');
+//     case 5: console.log('alo');
+//     default: console.log('not found');
+// }    8 - masala
